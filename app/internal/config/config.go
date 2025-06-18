0c2177cb35c027daa29bf7180f74ae6d91b57848
Cooper Weiser <cooper@Coopers-MacBook-Pro.local>
2025-06-17 18:04:32 -0700

    Update redis defaults

--- app/internal/config/config.go
+++ app/internal/config/config.go
@@ -12,7 +12,7 @@ func Init() {
 	viper.BindEnv("host", "HOST")
 
 	viper.SetDefault("postgres_connection_string", "postgres://postgres:postgres@postgres:5432/url_shortener?sslmode=disable")
-	viper.SetDefault("redis_connection_string", "redis:6379")
+	viper.SetDefault("redis_connection_string", "redis://redis:6379")
 	viper.SetDefault("port", "8080")
 	viper.SetDefault("environment", "production")
 	viper.SetDefault("host", "localhost")
